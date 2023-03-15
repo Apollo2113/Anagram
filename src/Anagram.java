4217675f4d25caab5adc7be2f4eaae8e7b9c347c
Apollo2113 <petrenkoroman1@gmail.com>
2023-03-15 09:40:15 +0200

    added new class Anagram

---
+++ src/Anagram.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+public class Anagram {
+    public static boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+
+        char[] sArray = s.toCharArray();
+        Arrays.sort(sArray);
+        char[] tArray = t.toCharArray();
+        Arrays.sort(tArray);
+
+        for (int i = 0; i < sArray.length; i++) {
+            if (sArray[i] != tArray[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
